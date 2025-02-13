ef6e65cd579c687fbfb10e9529f9e518a8f3949d
Brendan Cooney <101305939+b-cooney@users.noreply.github.com>
2025-02-13 15:02:49 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,6 @@ export default defineConfig({
               injectTo: "",
               children: `
                 \`;
-                document.body.appendChild(watermark);
               `,
             },
           ];
