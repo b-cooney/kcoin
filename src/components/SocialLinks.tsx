28cfd27d9b7dea349908ae2cac70664d79976239
Brendan Cooney <101305939+b-cooney@users.noreply.github.com>
2025-02-13 14:39:15 -0500

    Update SocialLinks.tsx

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -3,13 +3,13 @@ const SocialLinks = () => {
     <div className="flex flex-col items-center gap-4">
       <div className="flex flex-col gap-4 text-white text-center font-helvetica text-sm">
         <span className="hover:text-blue-200 cursor-pointer transition-colors">
-          warpcast
+          <a href="https://warpcast.com/~/channel/kcoin">warpcast</a>
         </span>
         <span className="hover:text-blue-200 cursor-pointer transition-colors">
-          twitter
+          <a href="https://x.com/hellokcoin">twitter</a>
         </span>
         <span className="hover:text-blue-200 cursor-pointer transition-colors">
-          giphy
+          <a href="https://giphy.com/channel/hellokcoin">giphy</a>
         </span>
       </div>
       <div className="font-['Press_Start_2P'] text-3xl">
